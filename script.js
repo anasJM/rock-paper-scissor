13d37ea64ac4251f107ec32edbabba35d4ad79a2
anasJM <anasjamlaoui@gmail.com>
2026-01-14 15:19:32 +0100

    adding getComputerChoice() function using Math.random

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello hello!")
+// console.log( Math.floor(Math.random() * 3) + 1 );
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch (randomNumber) {
+        case 1:
+            return "Rock";
+            break;
+
+        case 2:
+            return "Paper";
+            break;
+        
+        case 3:
+            return "Scissor";
+            break;
+    }
+}
+
+console.log(getComputerChoice());
